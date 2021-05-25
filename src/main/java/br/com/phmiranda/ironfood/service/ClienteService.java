93ec06119a18657f900fec7d9977dc35e2a3ef3e
phmiranda <pehhagah.1607@gmail.com>
2021-05-25 14:20:24 -0300

    [delete] - exclusão de dependências que não estão sendo utilizadas na camada de serviço.

--- src/main/java/br/com/phmiranda/ironfood/service/ClienteService.java
+++ src/main/java/br/com/phmiranda/ironfood/service/ClienteService.java
@@ -12,10 +12,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import javax.xml.ws.Response;
-import java.util.List;
-import java.util.Optional;
-
 @Service
 public class ClienteService {
 
